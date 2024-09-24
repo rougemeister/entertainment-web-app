2bab9d96bde694720a9fca1ab75740f1ec0ec9c4
rougemeister <rougemeister1@gmail.com>
2024-09-24 16:34:14 +0000

    Added sign-up component

---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss'
+})
+export class SignUpComponent {
+
+}
