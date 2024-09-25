e7c636f6b7af9cb689b390e6f65c5172b51912bd
rougemeister <rougemeister1@gmail.com>
2024-09-25 14:42:02 +0000

    Added data.json file and updated _mixins.scss file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 export const routes: Routes = [
     {path: '', component: SignInComponent},
-    {path: 'sign-up', component: SignUpComponent}
-];
+    {path: 'sign-up', component: SignUpComponent},
+    {path: 'homepage', component: HomepageComponent}  
+]
